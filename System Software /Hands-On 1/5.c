8724beb6d03efbfbb13bf122af09158e6a68ace2
Sage_2001 <somya.malgudi@gmail.com>
2023-08-12 12:00:04 +0530

    1-5 Hands-On Done

---
+++ System Software /Hands-On 1/5.c
@@ -0,0 +1,30 @@
+/*Write a program to create five new files with infinite loop. Execute the program in the background
+and check the file descriptor table at /proc/pid/fd.*/
+
+#include<stdio.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<fcntl.h>
+
+int main(){
+    int fd , count = 0;
+    while (1)
+    {
+        if(count<5){
+            fd = open("create_file", O_RDWR);
+            count++;
+            if (fd>=0)
+            {
+                printf("File opened with descriptor: %d\n", fd);
+            }
+            else{
+                printf("File opening failed\n");
+            }
+        }
+    }
+    return 0;
+}
+
+//not you have to run this in background
+// use ls-l to see the table
