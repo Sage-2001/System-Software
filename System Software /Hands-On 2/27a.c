e922d25f8cf82d5b71021be5a2be0e0b733bb588
Sage_2001 <somya.malgudi@gmail.com>
2023-10-09 09:52:40 +0530

    Done till 27

---
+++ System Software /Hands-On 2/27a.c
@@ -0,0 +1,44 @@
+/*
+============================================================================
+Name : 27_a.c
+Author : Somya Malgudi
+Description : Write a program to receive messages from the message queue.
+	a. with 0 as a flag
+============================================================================
+*/
+#include<sys/ipc.h>
+#include<sys/types.h>
+#include<sys/msg.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main(){
+    key_t key = ftok(".",'a');
+    if(key == -1){
+        perror("Failed to create a key");
+        exit(1);
+    }
+    int id = msgget(key,IPC_CREAT|0777);
+    if(id == -1){
+        perror("Failed to create the message queue\n");
+        exit(1);
+    }
+
+    struct msgbuf
+    {
+        long mtype;
+        int num;
+    };
+
+    struct msgbuf msg;
+    msg.mtype = 1;
+
+    int status = msgrcv(id,&msg,sizeof(msg),msg.mtype,0);
+    if (status == -1)
+    {
+        perror("Failed to send the message\n");
+        exit(1);
+    }
+    else    printf("Message recieved with number: %d ",msg.num);
+    return 0;
+}
