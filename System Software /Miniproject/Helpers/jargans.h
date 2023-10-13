b8951392377a38396e87bd69af96616e7455e9bc
Sage_2001 <somya.malgudi@gmail.com>
2023-10-13 11:01:22 +0530

    Changing port number

--- System Software /Miniproject/Helpers/jargans.h
+++ System Software /Miniproject/Helpers/jargans.h
@@ -1,7 +1,7 @@
 #ifndef jargans_h
 #define jargans_h
 
-#define PORT 2001
+#define PORT 2002
 
 #define Welcome printf("*********************************** Welcome Back to Academia :: Course Registration ***************************************** \nLogin Type ?\nEnter your choice {1.Admin, 2.Professor, 3.Student}");
 
