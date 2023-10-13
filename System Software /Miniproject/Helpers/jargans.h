edad6e7adacf8d205836d023da6474ffc89e90cb
Sage_2001 <somya.malgudi@gmail.com>
2023-10-13 10:16:07 +0530

    Implementing client and server

---
+++ System Software /Miniproject/Helpers/jargans.h
@@ -0,0 +1,8 @@
+#ifndef jargans_h
+#define jargans_h
+
+#define PORT 2001
+
+#define Welcome printf("*********************************** Welcome Back to Academia :: Course Registration ***************************************** \nLogin Type ?\nEnter your choice {1.Admin, 2.Professor, 3.Student}");
+
+#endif
---
+++ System Software /Miniproject/client.c
@@ -0,0 +1,62 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/ip.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#include "jargans.h"
+
+int main(){
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD == -1)
+    {
+        perror("failed to create socket\n");
+        exit(1);
+    }
+
+    struct socketaddr_in server_address;
+    client.sin_addr.s_addr = htonl(INADDR_ANY);
+    client.sin_family = AF_INET;
+    client.sin_port = htons(PORT);
+
+    int connectionStat = connect(socketFD, (struct socketaddr *) &server_address,sizeof(server_address));
+    if (connectionStat == -1)
+    {
+        perror("socket connection error");
+        exit(1);
+    }
+
+    Welcome;
+    int ch;
+    scanf("%c", &ch);
+    
+    while (1)
+    {
+        char buf[100];
+        switch (ch)
+        {
+        case 1:
+            buf = "admin";
+            write(socketFD, buf,sizeof(buf));
+            break;
+
+        case 2:
+            buf = "faculty";
+            write(socketFD, buf,sizeof(buf));
+            break;
+
+        case 3:
+            buf = "student";
+            write(socketFD, buf,sizeof(buf));
+            break;
+        
+        default:
+            "Invalid Choice";
+            exit(1);
+            break;
+        }
+    }
+    close(socketFD);
+    return 0;
+}
