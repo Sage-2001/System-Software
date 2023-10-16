2d0166f968409b8221c5b3da9e43555253555998
Sage_2001 <somya.malgudi@gmail.com>
2023-10-16 19:35:44 +0530

    Creating models

--- System Software /Miniproject/Helpers/jargans.h
+++
@@ -1,8 +0,0 @@
-#ifndef jargans_h
-#define jargans_h
-
-#define PORT 2002
-
-#define Welcome printf("*********************************** Welcome Back to Academia :: Course Registration ***************************************** \nLogin Type ?\nEnter your choice {1.Admin, 2.Professor, 3.Student}");
-
-#endif
